18023f385071945a33829240a7f71edfca3ca83d
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-09-30 18:11:21 -0400

    feat: pretty path handler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::ffi::OsString;
+use std::path::Path;
 
 use anyhow::Result;
 use owo_colors::OwoColorize;
@@ -6,32 +7,30 @@ use std::io::Write;
 use tabwriter::TabWriter;
 use winreg::enums::*;
 use winreg::RegKey;
-use winreg::RegValue;
 
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
+    /// Show raw (non-tty) output.
+    #[arg(long)]
+    raw: bool,
     #[command(subcommand)]
     command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
+    /// List out specific envs(s)
+    Show {
+        /// Specific envs(s) to list out
+        paths: Vec<OsString>,
     },
 }
 
-fn main() -> Result<()> {
-    let cli = Cli::parse();
-
-    let hklm = RegKey::predef(HKEY_CURRENT_USER);
-    let cur_ver = hklm.open_subkey("Environment")?;
+fn pretty_registry(hklm: RegKey, subkey: &str) -> Result<()> {
+    let cur_ver = hklm.open_subkey(subkey)?;
 
     if let Some((w, _)) = term_size::dimensions() {
         let mut tw = TabWriter::new(vec![]);
@@ -54,10 +53,13 @@ fn main() -> Result<()> {
                     name.blue(),
                     if longest_name_length + 2 + value.len() > w {
                         format!(
-                            "(too long - run {})",
-                            format!("`wenv show {}`", name.underline()).purple()
+                            "({}>{} characters - run {}{}",
+                            value.len(),
+                            w - longest_name_length - 2,
+                            format!("`wenv show {}`", name).purple(),
+                            ")".red()
                         )
-                        .yellow()
+                        .red()
                         .to_string()
                     } else {
                         value.green().to_string()
@@ -70,6 +72,77 @@ fn main() -> Result<()> {
         tw.flush().unwrap();
 
         println!("{}", String::from_utf8(tw.into_inner().unwrap()).unwrap());
+    }
+
+    Ok(())
+}
+
+fn registry(hklm: RegKey, subkey: &str) -> Result<()> {
+    let cur_ver = hklm.open_subkey(subkey)?;
+
+    for (name, value) in cur_ver.enum_values().map(|x| x.unwrap()) {
+        println!("{}: {}", name, value);
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let is_tty = atty::is(atty::Stream::Stdout) && !cli.raw;
+
+    match cli.command {
+        None => {
+            let hklm = RegKey::predef(HKEY_CURRENT_USER);
+
+            if is_tty {
+                pretty_registry(hklm, "Environment")?;
+            } else {
+                registry(hklm, "Environment")?;
+            }
+        }
+        Some(Commands::Show { paths }) => {
+            let hklm = RegKey::predef(HKEY_CURRENT_USER);
+            let cur_ver = hklm.open_subkey("Environment")?;
+
+            let values: Vec<(&OsString, OsString)> = paths
+                .iter()
+                .map(|x| (x, cur_ver.get_value(x).unwrap()))
+                .collect::<Vec<_>>();
+
+            for (key, value) in values {
+                let key = key.to_str().unwrap();
+                println!("{}", key.blue());
+
+                if key == "Path" {
+                    let mut problem_count = 0;
+                    let path = value.to_str().unwrap();
+                    let path = path.split(";").collect::<Vec<_>>();
+                    for path_str in path {
+                        let path = Path::new(path_str);
+
+                        // check if path exists
+                        if !path.exists() {
+                            println!("{}", format!("  {} {}", path_str, "(does not exist)").red());
+                            problem_count += 1;
+                            continue;
+                        }
+
+                        println!("  {}", path.to_str().unwrap());
+                    }
+
+                    if problem_count > 0 {
+                        println!("{}", format!("{} problems found", problem_count).red());
+                    } else {
+                        println!("{}", "0 problems found".green());
+                    }
+
+                    continue;
+                }
+
+                println!("{:?}", value);
+            }
+        }
     }
 
     Ok(())
