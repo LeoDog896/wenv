569b35df88a79327151e7d9fa993c613ccfa2ea6
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-09-30 17:36:47 -0400

    feat: colored output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::ffi::OsString;
 
 use anyhow::Result;
+use owo_colors::OwoColorize;
 use std::io::Write;
 use tabwriter::TabWriter;
 use winreg::enums::*;
@@ -32,19 +33,44 @@ fn main() -> Result<()> {
     let hklm = RegKey::predef(HKEY_CURRENT_USER);
     let cur_ver = hklm.open_subkey("Environment")?;
 
-    let mut tw = TabWriter::new(vec![]);
+    if let Some((w, _)) = term_size::dimensions() {
+        let mut tw = TabWriter::new(vec![]);
 
-    for (name, value) in cur_ver.enum_values().map(|x| x.unwrap()) {
-        let value = match value.vtype {
-            REG_SZ | REG_EXPAND_SZ => format!("{}", value),
-            _ => unimplemented!("unimplemented type: {:?}", value.vtype),
-        };
-        tw.write(format!("{}\t{}\n", name, value).as_bytes())?;
+        let longest_name_length = cur_ver
+            .enum_values()
+            .map(|x| x.unwrap())
+            .map(|(name, _)| name.len())
+            .max()
+            .unwrap();
+
+        for (name, value) in cur_ver.enum_values().map(|x| x.unwrap()) {
+            let value = match value.vtype {
+                REG_SZ | REG_EXPAND_SZ => format!("{}", value),
+                _ => unimplemented!("unimplemented type: {:?}", value.vtype),
+            };
+            tw.write(
+                format!(
+                    "{}\t{}\n",
+                    name.blue(),
+                    if longest_name_length + 2 + value.len() > w {
+                        format!(
+                            "(too long - run {})",
+                            format!("`wenv show {}`", name.underline()).purple()
+                        )
+                        .yellow()
+                        .to_string()
+                    } else {
+                        value.green().to_string()
+                    }
+                )
+                .as_bytes(),
+            )?;
+        }
+
+        tw.flush().unwrap();
+
+        println!("{}", String::from_utf8(tw.into_inner().unwrap()).unwrap());
     }
-
-    tw.flush().unwrap();
-
-    println!("{}", String::from_utf8(tw.into_inner().unwrap()).unwrap());
 
     Ok(())
 }
